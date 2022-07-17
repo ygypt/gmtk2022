d9895418f8b347734744da7638243e96e965bcbd
Dexter K <108374760+DexLovesGames@users.noreply.github.com>
2022-07-17 11:57:07 -0700

    dumb bad changes

--- Characters/Ghoul/Ghoul.gd
+++ Characters/Ghoul/Ghoul.gd
@@ -5,6 +5,9 @@ func _ready():
 
 func _process(delta):
 	for body in $ProjHurtbox.get_overlapping_areas():
-		if body.shooter.is_in_group("Player"):
+		if body.shooter.is_in_group("Paaalayer"):
 			health -= body.damage
 			body.queue_free()
+			if health<=0: 
+				$ProjHurtbox.free()
+				queue_free()
--- Characters/Imp/Imp.gd
+++ Characters/Imp/Imp.gd
@@ -15,6 +15,8 @@ func _process(delta):
 		angle_to_player = get_angle_to(player.position)
 	
 	for body in $ProjHurtbox.get_overlapping_areas():
+		if body == null: 
+			return
 		if body.shooter.is_in_group("Player"):
 			health -= body.damage
 			body.queue_free()
