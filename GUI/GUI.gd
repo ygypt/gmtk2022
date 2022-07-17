61925e25854a8234b2a35470b7a7af15f0157b9e
Dexter K <108374760+DexLovesGames@users.noreply.github.com>
2022-07-17 10:24:32 -0700

    GUI

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -14,6 +14,15 @@ func step(player : Player):
 	var current_hp = player.health
 	var ammo_left = player. weapon.current_ammo
 	var ammo_total = player.weapon.total_ammo
+	$Control/HBoxContainer/AmmoLeftLabel.text= str(ammo_left)
+	$Control/HBoxContainer/AmmoTotalLabel.text= str(ammo_total)
+	if ammo_left < ammo_total*.5:
+		$Control/HBoxContainer/AmmoLeftLabel.add_color_override("font_color",Color("de2020")) #RED
+	else:
+		$Control/HBoxContainer/AmmoLeftLabel.add_color_override("font_color",Color("ffffff")) #WHITE
+	if not weaponheld.reload_timer.is_stopped():
+		$Control/HBoxContainer/AmmoLeftLabel.text= "Reloading... "+ str(0)
+		$Control/HBoxContainer/AmmoLeftLabel.add_color_override("font_color",Color("de2020"))
 	if (not ANIMS.is_playing()) && (current_hp >= 4):
 		ANIMS.play("HeathFourIdle")
 		print("TESTINGTESTING")
@@ -22,6 +31,8 @@ func step(player : Player):
 		elif current_hp == 2: ANIMS.play_backwards("HealthTwoToThree")
 		elif current_hp == 1: ANIMS.play_backwards("HealthOneToTwo")
 		elif current_hp == 0: ANIMS.play_backwards("HealthStart")
+	
+	
 	
 	
 	#this happens last
