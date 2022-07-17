825f18458a8340b7915466cd7c0e8f2ac73ee629
Dexter K <108374760+DexLovesGames@users.noreply.github.com>
2022-07-16 22:21:07 -0700

    few things

--- Characters/Player/States/Move.gd
+++ Characters/Player/States/Move.gd
@@ -7,6 +7,11 @@ func enter(entity: Player) -> Node:
 func step(entity) -> Node:
 	entity.velocity = entity.move_vector * entity.speed
 	
+	if entity.move_vector.y >= 0:
+		entity.anims.play("Move_down")
+	else:
+		entity.anims.play("Move_up")
+		
 	if not entity.move_vector:
 		return $"../Idle"
 		
